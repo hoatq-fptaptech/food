d815f9a3b923e86f50443ad3b74558a1bb725330
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-09-15 17:53:46 +0700

    add code

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',//env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'ec2-3-226-231-4.compute-1.amazonaws.com',//env('DB_HOST', '127.0.0.1'),
+            'port' => '5432'//env('DB_PORT', '5432'),
+            'database' => 'd1hp5l5ruj4u64',//env('DB_DATABASE', 'forge'),
+            'username' => 'iaomdxsnxfqjar',//env('DB_USERNAME', 'forge'),
+            'password' => '43b8a5b0ab57f5bfddc65d8cfeeae04151a7b8d2a5e44a4d55e7526723e5cc3a',//env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
