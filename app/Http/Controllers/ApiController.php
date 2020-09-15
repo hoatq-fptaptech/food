3e3481c323a0cbe5f38682b5463f5c1d1b941517
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-09-15 21:25:36 +0700

    add code

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,6 +7,61 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     public function menu(){
-        return "a";
+        return response()->json([
+            "message"=>"success",
+            "data"=>[
+                [
+                    "id"=>1,
+                    "icon"=>"https://www.lotteria.vn/media/catalog/tmp/category/cate-9_1.jpg",
+                    "name"=>"Burger"
+                ],
+                [
+                    "id"=>2,
+                    "icon"=>"https://www.lotteria.vn/media/catalog/tmp/category/cate-2.jpg",
+                    "name"=>"Chicken"
+                ],
+                [
+                    "id"=>3,
+                    "icon"=>"https://www.lotteria.vn/media/catalog/tmp/category/cate-7.jpg",
+                    "name"=>"Rice"
+                ],
+                [
+                    "id"=>4,
+                    "icon"=>"https://www.lotteria.vn/media/catalog/tmp/category/cate-6.jpg",
+                    "name"=>"Drinks"
+                ],
+                [
+                    "id"=>5,
+                    "icon"=>"https://www.lotteria.vn/media/catalog/tmp/category/cate-8.jpg",
+                    "name"=>"Dessert"
+                ],
+            ]
+        ]);
+    }
+
+    public function today(){
+         return response()->json([
+                    "message"=>"success",
+                    "data"=>[
+                        [
+                            "name"=>"Burger Bulgogi Phần (Bulgogi Burger Value)",
+                            "image"=>"https://dscnnwjxnwl3f.cloudfront.net/media/catalog/product/cache/584039753b87a8d227764e04fc461e3e/v/a/value_bulgogi_4.png",
+                            "description"=>"",
+                            "price"=>81000
+                        ],
+                        [
+                            "name"=>"Combo Burger Tôm (Shrimp Burger Combo)",
+                            "image"=>"https://dscnnwjxnwl3f.cloudfront.net/media/catalog/product/cache/584039753b87a8d227764e04fc461e3e/c/o/combo_shrimp_4.png",
+                            "description"=>"",
+                            "price"=>77000
+                        ],
+                        [
+                            "name"=>"Gà Rán Phần (Fried Chicken Set)",
+                            "image"=>"https://dscnnwjxnwl3f.cloudfront.net/media/catalog/product/cache/584039753b87a8d227764e04fc461e3e/p/r/product_transparent.png",
+                            "description"=>"",
+                            "price"=>83000
+                        ],
+                    ]
+                    ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get("/menu","ApiController@menu");
+Route::get("/today-special","ApiController@today");
